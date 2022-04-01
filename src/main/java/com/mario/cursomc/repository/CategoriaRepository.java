57a9e261e22243ccb5c3cd17c526d3aad5e9b984
Mário Becker <mario.roberto1@hotmail.com>
2022-04-01 11:53:20 -0300

    api feactures

---
+++ src/main/java/com/mario/cursomc/repository/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.mario.cursomc.repository;
+
+import com.mario.cursomc.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
---
+++ src/main/java/com/mario/cursomc/services/CategoriaService.java
@@ -0,0 +1,19 @@
+package com.mario.cursomc.services;
+
+import com.mario.cursomc.domain.Categoria;
+import com.mario.cursomc.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public Categoria find(Integer id) {
+        Optional<Categoria> obj = categoriaRepository.findById(id);
+        return obj.orElse(null);
+    }
+}
